dc16b5143da7f4587896ef42ab7301fe9cb62797
Rigoberto L. Perez <rlperez@udel.edu>
2018-06-19 23:00:34 -0400

    Add csv value write function

--- src/main/kotlin/FileUtil.kt
+++ src/main/kotlin/FileUtil.kt
@@ -7,4 +7,11 @@ class FileUtil {
                 .filter { s -> s.isFile && s.path.endsWith(".csv") }
                 .toList()
     }
+
+    fun writeValueCountCsv(path: String, data: Map<String, Int>): Unit {
+        File(path).printWriter().use { out ->
+            out.println("value,count")
+            data.entries.forEach { it -> out.println("${it.key},${it.value}") }
+        }
+    }
 }
