c32c618263562518d3e01232d2e1fcbf4930bd2e
Rigoberto L. Perez <rlperez@udel.edu>
2018-06-20 00:38:41 -0400

    Add executions of solutions

--- src/main/kotlin/ScreenApplication.kt
+++ src/main/kotlin/ScreenApplication.kt
@@ -1,5 +1,20 @@
 class ScreenApplication
 
 fun main(args: Array<String>) {
-    val csvPaths = FileUtil().getCsvFiles(args[0])
+    val fileUtil = FileUtil()
+    val files = fileUtil.getCsvFiles(args[0])
+
+    println("Executing solution for question 1...")
+    val solution1 = Solution1()
+    println(solution1.averageNumberOfFieldsPerCsv(files))
+
+    println("Executing solution for question 2...")
+    val solution2 = Solution2()
+    val wordCounts = solution2.wordCountOfAllCsvs(files)
+    println("Writing output to ./solution2.csv")
+    fileUtil.writeValueCountCsv("./solution2.csv", wordCounts)
+
+    println("Executing solution for question 3...")
+    val solution3 = Solution3()
+    println(solution3.totalNumberOfRowsInCsvs(files))
 }
